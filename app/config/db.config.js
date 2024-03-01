c7ad387be840808cff217a3f4239bbccf72a8433
siavash <sepahisiavash@gmail.com>
2024-03-01 12:38:46 +0330

    first init

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "Awid_Alnoor",
+  PASSWORD: "Puya@Tehran@123",
+  DB: "awid_alnoor"
+};
---
+++ app/models/categories.model.js
@@ -0,0 +1,128 @@
+const sql = require("./db.js");
+
+// constructor
+const Category = function(category) {
+  this.title = category.title;
+  this.description = category.description;
+};
+
+Category.create = (newCategory, result) => {
+  sql.query("INSERT INTO categories SET ?", newCategory, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created tutorial: ", { id: res.insertId, ...newTutorial });
+    result(null, { id: res.insertId, ...newTutorial });
+  });
+};
+
+Category.findById = (id, result) => {
+  sql.query(`SELECT * FROM tutorials WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found tutorial: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found Tutorial with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+Category.getAll = (title, result) => {
+  let query = "SELECT * FROM categories";
+
+  if (title) {
+    query += ` WHERE title LIKE '%${title}%'`;
+  }
+
+  sql.query(query, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("categories: ", res);
+    result(null, res);
+  });
+};
+
+Category.getAllPublished = result => {
+  sql.query("SELECT * FROM tutorials WHERE published=true", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("tutorials: ", res);
+    result(null, res);
+  });
+};
+
+Category.updateById = (id, tutorial, result) => {
+  sql.query(
+    "UPDATE tutorials SET title = ?, description = ?, published = ? WHERE id = ?",
+    [tutorial.title, tutorial.description, tutorial.published, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found Tutorial with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated tutorial: ", { id: id, ...tutorial });
+      result(null, { id: id, ...tutorial });
+    }
+  );
+};
+
+Category.remove = (id, result) => {
+  sql.query("DELETE FROM tutorials WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found Tutorial with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted tutorial with id: ", id);
+    result(null, res);
+  });
+};
+
+Category.removeAll = result => {
+  sql.query("DELETE FROM tutorials", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`deleted ${res.affectedRows} tutorials`);
+    result(null, res);
+  });
+};
+
+module.exports = Category;
